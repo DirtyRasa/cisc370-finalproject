f88ff0c00a9f4c9c23af333e9a00ed79ed7eff53
Craig Martin <cmgecola@gmail.com>
2011-04-19 18:37:41 -0500

    Fixed the connection thread to accept more than one connection at a time

--- game_server_c/src/game/server/c/GameServer.java
+++ game_server_c/src/game/server/c/GameServer.java
@@ -1,18 +1,24 @@
 package game.server.c;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class GameServer {
 
-	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		GameConnectionThread gameConnectionThread = new GameConnectionThread();
-		gameConnectionThread.start();
-		
-		while (true) {		
-			try {
-				gameConnectionThread.sleep(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		try {
+			ServerSocket server = new ServerSocket(80);
+			System.out.println("Game server started and listening on port 80");
+			while(!Thread.currentThread().isInterrupted()) {
+				Socket socket = server.accept();
+				System.out.println("Received ping from " + socket.getInetAddress());
+				GameConnectionThread gameConnectionThread = new GameConnectionThread(socket);
+				gameConnectionThread.start();
 			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
