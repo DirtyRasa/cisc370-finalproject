0c899c6934003eaaf7e8c5041b1c6a6397e707b1
Craig Martin <cmgecola@gmail.com>
2011-04-29 11:28:04 -0500

    Updated database to now work with the username/login after the new install script is run...
    Fixed the Blackjack handshake thread to not ask if you want to play blackjack when you already selected to play it

--- game_server_console/src/blackjack/server/BlackjackHandshakeThread.java
+++ game_server_console/src/blackjack/server/BlackjackHandshakeThread.java
@@ -3,8 +3,6 @@ package blackjack.server;
 import game.server.User;
 
 import communication.Communication;
-import communication.Response;
-import communication.ResponseException;
 
 public class BlackjackHandshakeThread extends Thread{
 	Blackjack _blackjack;
@@ -17,36 +15,17 @@ public class BlackjackHandshakeThread extends Thread{
 	}
 	
 	public void run(){
-		boolean done = false;
 		try{
-			while(!done){
-				Communication.sendQuestion(_user.getOutput(), "\nWould you like to play Blackjack (y/n)?");
-				try{
-					if(Response.binaryEval(_user.getInput().readLine()))
-					{	
-						RemotePlayer player = new RemotePlayer(_user.getName(), _user.getSocket(), _user.getOutput(), _user.getInput());
-						
-						done=true;
-	
-						Communication.sendMessage(_user.getOutput(), "\nPlease wait for the current hand to finish.");
-	
-						try
-						{
-							_blackjack.addPlayer(player);
-							System.out.println("Player " + player.getName() +" added.");
-						}
-						catch(Exception e){throw new TableFullException("Table is currently full");}
-					}
-					else
-					{
-						Communication.sendMessage(_user.getOutput(), "end");
-						done = true;
-					}
-				}catch (ResponseException ex){
-					Communication.sendMessage(_user.getOutput(), ex.getMessage());
-					done = false;
-				}
+			RemotePlayer player = new RemotePlayer(_user.getName(), _user.getSocket(), _user.getOutput(), _user.getInput());
+
+			Communication.sendMessage(_user.getOutput(), "\nPlease wait for the current hand to finish.");
+
+			try
+			{
+				_blackjack.addPlayer(player);
+				System.out.println("Player " + player.getName() +" added.");
 			}
+			catch(Exception e){throw new TableFullException("Table is currently full");}
 		}
 		catch(Exception e){throw new RuntimeException();}
 	}
