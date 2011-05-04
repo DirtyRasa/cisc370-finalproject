b14340f5e943081fc5dca0306e76d1844a711b2a
Craig Martin <cmgecola@gmail.com>
2011-05-03 19:05:25 -0500

    Updates DAL and added money functions to user

--- game_server_console/src/game/server/User.java
+++ game_server_console/src/game/server/User.java
@@ -4,37 +4,63 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import dal.DataAccessLayer;
+
 public class User {
 	private Socket _client;
-	PrintWriter _out;
-	BufferedReader _in;
-	String _name;
+	private PrintWriter _out;
+	private BufferedReader _in;
+	private String _userName;
+	private double _money;
+	
+	private DataAccessLayer _dal;
 
-	public User(String name, Socket client, PrintWriter out, BufferedReader in)
+	public User(String userName, Socket client, PrintWriter out, BufferedReader in)
 	{
+		_dal = new DataAccessLayer();
 		_client = client;
 		_out = out;
 		_in = in;
-		_name = name;
+		_userName = userName;
+		try {
+			_money = _dal.getMoney(_userName);
+		} catch (Exception e) {
+			//Should never get here.
+		}
 	}
 
-	public Socket getSocket()
-	{
-		return _client;
-	}
+	public Socket getSocket()  { return _client; }
 
-	public PrintWriter getOutput()
-	{
-		return _out;
-	}
+	public PrintWriter getOutput()  { return _out; }
 
-	public BufferedReader getInput()
+	public BufferedReader getInput()  { return _in; }
+	
+	public String getName()  { return _userName; }
+	
+	public double getMoney() { return _money; }
+	
+	public boolean addMoney(double add)
 	{
-		return _in;
+		_money += add;
+		try {
+			_dal.setMoney(_userName, _money);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
-	public String getName()
+	//Should we allow -money? probably not Handle elsewhere?
+	public boolean subtractMoney(double subtract)
 	{
-		return _name;
+		_money -= subtract;
+		try {
+			_dal.setMoney(_userName, _money);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
