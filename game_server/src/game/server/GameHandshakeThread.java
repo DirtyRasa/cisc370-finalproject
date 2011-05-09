e49f65930102362dd37a7a7bfa737b2f64a7474f
Craig Martin <cmgecola@gmail.com>
2011-05-09 13:15:43 -0500

    IT WORKS

--- game_server/src/game/server/GameHandshakeThread.java
+++ game_server/src/game/server/GameHandshakeThread.java
@@ -28,41 +28,14 @@ public class GameHandshakeThread extends Thread
 			
 			user = new User(_client, out, in);
 			
-			System.out.println("User created. Sending welcome message");
-			
-			Communication.sendMessage(user, "***** Welcome to the Game Server *****");
-			
 			do
 			{
-				Communication.sendQuestion(user, "Do you already have an account? (Y/N) ");
 				try{
 					user = Response.binaryEval(in.readLine()) ? _gs.login(user) : _gs.register(user);
 					if(user != null)
 						done = true;
 					else
-						user = new User(_client, out, in); 
-					if(done)
-						Communication.sendMessage(user, "Thank you, press enter to continue.");
-					/*
-					int response = Response.trinaryEval(user.getInputWithTimeout());
-					
-					switch(response)
-					{
-					case -1:
-						_gs.register(user);
-						break;
-					case 0:
-						_gs.logout(user);
-						break;
-					case 1:
-						_gs.login(user);
-						break;
-					}					
-					
-					if(user != null)
-						done = true;
-					if(done)
-						Communication.sendMessage(user, "Thank you, press enter to continue.");*/
+						user = new User(_client, out, in);
 				}
 				catch (ResponseException ex){
 					//TODO In case user is null; Just create a new user object.
@@ -71,8 +44,6 @@ public class GameHandshakeThread extends Thread
 				}
 				
 			}while(!done);
-			Communication.sendQuestion(user, "");
-			in.readLine();
 			
 			GameSelectionThread gameSelectionThread = new GameSelectionThread(_gs, user);
 			gameSelectionThread.start();			
